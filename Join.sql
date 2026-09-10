1da4515b446ea0da720c79f37325f8e95acddec5
niranjan122005 <niranjanv@thestackly.com>
2026-09-10 12:42:44 +0530

    Actions, Cross Join

--- Join.sql
+++ Join.sql
@@ -26,3 +26,33 @@ INSERT INTO iplTeam (name,playerId) values
 
 SELECT * from cricketer c inner join iplteam i  on c.id=i.playerId
 inner join jersey j on c.id=j.playerId;
+
+
+select * from actor;
+select * from movies;
+
+create table movies(
+id int primary key auto_increment,
+name varchar(30) not null,
+actorId int,
+foreign key (actorId) references actor(id)
+ON DELETE restrict
+ON update restrict
+);
+
+
+SELECT * from cricketer cross join iplteam;
+
+DROP TABLE movies;
+
+insert into movies (name, actorId) values
+
+('Thunivu',3),
+('Jailer',7),
+('Ponniyin Selvan',4),
+('Vikram',1),
+('Ponniyin Selvan 2',4)
+;
+
+UPDATE actor SET id=20 where id=2;
+delete from actor where id=3;
